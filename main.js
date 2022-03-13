57048d5b2dd60e8ddefeaabdac15af2906d681b8
Mr.GeschVed <86187817+chilledboyzrblx@users.noreply.github.com>
2022-03-13 18:13:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,22 +8,37 @@ answer_holder = "";
 score = 0;
 
 function draw() {
-
+    check_sketch();
+    answer_holder="set";
+    score++
+    document.getElementById("score").innerHTML = "Score: "+score
 }
 
-function check_sketch() {
 
+
+function check_sketch(){
+    timer_counter = timer_counter+1;
+    document.getElementById("timer").innerHTML = "Time: "+timer_counter+" second(s)"
+    console.log(timer_counter);
+    if(timer_counter<=400){
+        timer_counter=0;
+        timer_check="completed"
+    }
+    if(answer_holder=="set" || timer_check=="completed"){
+        timer_check="";
+        answer_holder="";
+        updateCanvas();
+    }
 }
+
 
 function updateCanvas() {
     random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
     console.log(quick_draw_data_set[random_number]);
-
     document.getElementById("sketch_to_be_drawn").innerHTML = "Sketch To Be Drawn: " + quick_draw_data_set[random_number];
 }
 
-function setup(){
+function setup() {
     canvas = createCanvas(280, 280);
     canvas.center();
-    
 }
